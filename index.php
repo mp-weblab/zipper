41880213667081bae2a6b2d683a89192ee6e88e7
Crabouille777 <ton.email@example.com>
2026-02-07 18:35:16 -0500

    Update empty index.php to prevent direct file access

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php 
 // Ce fichier est plus vide que le frigo d’un dev en fin de mois.
+// This file is emptier than a developer's fridge at the end of the month.
+// Este archivo está más vacío que el refrigerador de un desarrollador a final de mes.
 ?>
